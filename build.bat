3c04b4a42a8dfd53683d1c27a69e6c6e0c90ec0a
dejikcp <96624782+dejikcp@users.noreply.github.com>
2022-05-08 15:11:37 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,6 +4,9 @@ SET GOOS=linux
 SET GOARCH=amd64
 go build -o webBenchmark_linux_x64
 SET GOOS=linux
+SET GOARCH=s390x
+go build -o webBenchmark_linux_s390x
+SET GOOS=linux
 SET GOARCH=arm
 go build -o webBenchmark_linux_arm
 SET GOOS=windows
